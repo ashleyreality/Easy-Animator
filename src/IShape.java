04f13fb594e7acf9fa13be53b9423d4383549fdb
ashley <51171130+ashleyreality@users.noreply.github.com>
2021-03-30 14:51:17 -0400

    Updating IShape

--- src/IShape.java
+++ src/IShape.java
@@ -1,2 +1,22 @@
+import java.awt.Color;
+import java.awt.geom.Point2D;
+
+/**
+ * This interface describes the methods required to create a Shape class.
+ */
 public interface IShape {
+
+  /**
+   * Returns the current color of a shape.
+   *
+   * @return a Color associated with a shape
+   */
+  Color getColor();
+
+  /**
+   * Returns the current location of a shape.
+   *
+   * @return a Point2D location
+   */
+  Point2D getLocation();
 }
