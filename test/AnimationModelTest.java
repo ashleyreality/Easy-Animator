f9a23d630cdd79f65b5aa850583a139eb7fcfb5d
ashley <51171130+ashleyreality@users.noreply.github.com>
2021-03-30 21:11:34 -0400

    updating tests a bit

--- test/AnimationModelTest.java
+++ test/AnimationModelTest.java
@@ -10,40 +10,50 @@ public class AnimationModelTest {
 
   @Test
   public void TextTest() {
-    // create new shapes first
-    // give shape a point2D
+    // create a Point2D for each shape, then create the shape
     Point2D rLocation1 = new Point2D.Double(200.0, 200.0);
     IShape R = new Rectangle(Color.red, rLocation1, 50.0, 100.0);
+
     Point2D cLocation = new Point2D.Double(500.0, 100.0);
     IShape C = new Oval(Color.blue, cLocation, 60.0, 30.0);
-    // make a new movement with to location
-    // and ticks when it moves
+
+    // move each shape to a new location
     Point2D rLocation2 = new Point2D.Double(300.0, 300.0);
     MoveShape move1 = new MoveShape(R, 1, 100, rLocation1, rLocation2);
+
     Point2D cNewLocation = new Point2D.Double(500.0, 400.0);
-    MoveShape move2 = new MoveShape(C,10, 50, cLocation, cNewLocation, );
-    // make new shape change
-    // and ticks when it changes
-    // change color
+    MoveShape move2 = new MoveShape(C,10, 50, cLocation, cNewLocation);
+
+    // change a shape's color
     ChangeColor change1 = new ChangeColor(C, 50, 75, Color.blue, Color.green);
-    //change width and height
+
+    //change a shape's width and height
     ScaleShape change2 = new ScaleShape(R, 25, 100, 50,
             100, 5, 10);
-    //another move for R, back to original location
+
+    //move shape R back to original location
     MoveShape move3 = new MoveShape(R, 110, 150, rLocation2, rLocation1);
+
+    // create the animation model
     testAnimation = new AnimationModelImpl();
+
     // add the shapes to a shape list in the animation model
     // decide when they appear and disappear here
+    // fixme -- where does timing happen?
     // rectangle appears at 1, disappears at 100
     testAnimation.addShape(R, 1, 100);
     testAnimation.addShape(C, 6, 100);
+
     // includes timing of changes
+    // add shape changes to animation model
     testAnimation.addChange(move1, 10, 50);
     testAnimation.addChange(move2, 20, 70);
     testAnimation.addChange(change1, 50, 80);
     testAnimation.addChange(change2, 51, 70);
     testAnimation.addChange(move3, 70, 100);
+
     // maybe in the toString method?
+
     // then get the string of appearance, disappearance, and moves and changes
     assertEquals("\n" +
                     "Shapes:\n" +
