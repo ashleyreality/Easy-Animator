e9b5849cefe26d2a60172860dba564400de5a215
ashley <51171130+ashleyreality@users.noreply.github.com>
2021-04-02 14:56:09 -0400

    Refactoring animation test class

--- test/AnimationModelTest.java
+++ test/AnimationModelTest.java
@@ -8,60 +8,51 @@ import static org.junit.Assert.assertEquals;
  */
 public class AnimationModelTest {
   private IAnimationModel testAnimation;
-  private Point2D rLocation;
-  private Point2D rNewLocation;
-  private Point2D cLocation;
-  private Point2D cNewLocation;
-
-  Color red;
-  Color blue;
-  Color green;
 
   IShape R;
   IShape C;
+
+  IEvent move1;
+  IEvent move2;
+  IEvent move3;
+
+  IEvent size1;
+
+  IEvent colorChange1;
 
   @Before
   public void setUp() {
     // create the animation model
     testAnimation = new AnimationModelImpl();
+
+    // create the shapes
     R = new Rectangle(50.0, 100.0, 1,0,0,200.0,200.0);
     C = new Oval(60.0, 30.0, 0, 0, 1, 500.0, 100.0);
 
-    Point2D rNewLocation = new Point2D(300.0, 300.0);
+    // create the moves
+    move1 = new MoveShape(R, 10, 50, 300.0, 300.0);
+    move2 = new MoveShape(C,20, 70, 500.0, 400.0);
+    move3 = new MoveShape(R, 70, 100, 200.0, 200.0);
 
-    Point2D cNewLocation = new Point2D(500.0, 400.0);
+    // create the size change
+    size1 = new ScaleShape();
+
+    // create the color change
+    colorChange1 = new ChangeColor();
 
   }
 
   @Test
   public void testAddShape() {
-
-    // add the shapes to a shape list in the animation model
-    // decide when they appear and disappear here
-    // rectangle appears at 1, disappears at 100
-
-    // Get the color of the Shape
-   // testAnimation.getColor();
+    // add the shapes to the animation
     testAnimation.addShape(R, 1, 100);
-
     testAnimation.addShape(C, 6, 100);
 
     assertEquals("", testAnimation.toString());
   }
 
   @Test
-  public void testAddSizeChange() {
-    testAnimation.addEvent("R", 25.0, 100.0, 51,
-            70);
-
-    assertEquals("", testAnimation.toString());
-  }
-
-  @Test
   public void testAddMove() {
-    IEvent move1 = new MoveShape(R, 10, 50, 300.0, 300.0);
-    IEvent move2 = new MoveShape(C,20, 70, 500.0, 400.0);
-    IEvent move3 = new MoveShape(R, 70, 100, 200.0, 200.0);
 
     testAnimation.addEvent(move1, 10, 50);
     testAnimation.addEvent(move2, 20, 70);
@@ -72,59 +63,26 @@ public class AnimationModelTest {
   }
 
   @Test
-  public void testColorChange() {
+  public void testAddSizeChange() {
+    testAnimation.addEvent();
 
+    assertEquals("", testAnimation.toString());
   }
 
   @Test
-  public void TextTest() {
+  public void testColorChange() {
+    testAnimation.addEvent();
 
-    // create a Point2D for each shape, then create the shape
-    Point2D rLocation1 = new Point2D.Double(200.0, 200.0);
-    IShape R = new Rectangle("R", Color.RED, rLocation1, 1, 100,50.0, 100.0);
+    assertEquals("", testAnimation.toString());
+  }
 
-    Point2D cLocation = new Point2D.Double(500.0, 100.0);
-    IShape C = new Oval("C", Color.BLUE, cLocation, 6, 100, 60.0, 30.0);
+  @Test
+  public void testEverything() {
+    testAddShape();
+    testAddMove();
+    testColorChange();
+    testAddSizeChange();
 
-    // move each shape to a new location
-    Point2D rLocation2 = new Point2D.Double(300.0, 300.0);
-    MoveShape move1 = new MoveShape(R, 1, 100, rLocation1, rLocation2);
-
-    Point2D cNewLocation = new Point2D.Double(500.0, 400.0);
-
-
-    // change a shape's color
-    ChangeColor change1 = new ChangeColor(C, 50, 75, Color.BLUE, Color.GREEN);
-
-    //change a shape's width and height
-    ScaleShape change2 = new ScaleShape(R, 25, 100, 50,
-            100, 5, 10);
-
-    //move shape R back to original location
-    MoveShape move3 = new MoveShape(R, 110, 150, rLocation2, rLocation1);
-
-    // create the animation model
-    testAnimation = new AnimationModelImpl();
-
-    // add the shapes to a shape list in the animation model
-    // decide when they appear and disappear here
-    // rectangle appears at 1, disappears at 100
-    //fixme -- commented the below two lines out so the test would run
-    //testAnimation.addShape("R", Color.RED, rLocation1, 1, 100,50.0, 100.0);
-    //testAnimation.addShape(C);
-
-    // includes timing of changes
-    // add shape changes to animation model
-
-    testAnimation.addMove("R", rLocation2, 10, 50);
-    testAnimation.addMove("C", cNewLocation, 20, 70);
-    testAnimation.addColorChange("C", Color.BLUE, 50, 80);
-    testAnimation.addSizeChange("R", 25.0, 100.0, 51, 70);
-    testAnimation.addMove("R", rLocation1, 70, 100);
-
-    // maybe in the toString method?
-
-    // then get the string of appearance, disappearance, and moves and changes
     assertEquals("\n" +
                     "Shapes:\n" +
                     "Name: R\n" +
@@ -149,6 +107,5 @@ public class AnimationModelTest {
                     "Height: 100.0 from t=51 to t=70\n" +
                     "Shape R moves from (300.0,300.0) to (200.0,200.0) from t=70 to t=100"
             , testAnimation.toString());
-    // something like that
   }
 }
