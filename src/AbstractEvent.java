e63b38cf283a97d02bf04a2787a4a4d4c1b401fb
ashley <51171130+ashleyreality@users.noreply.github.com>
2021-04-02 11:57:41 -0400

    Updating IEvent and its children

--- src/AbstractEvent.java
+++ src/AbstractEvent.java
@@ -21,4 +21,8 @@ public abstract class AbstractEvent implements IEvent {
   public int getEventEnd() {
     return eventEnd;
   }
+
+  public void change() {
+    // this method is a stub for now, until we have more information about how changes will work
+  }
 }
