ffe355a54855fd64f6b10f4693c541edb53ffa6f
aya :) <ayagare@gmail.com>
2021-04-03 17:29:21 -0400

    Updated AnimationModelTest & EventTest to account for corrections of "before" & "after" event values. Also updated JavaDocs for these 2 tests.

--- test/AnimationModelTest.java
+++ test/AnimationModelTest.java
@@ -1,4 +1,3 @@
-import org.hamcrest.core.Is;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,114 +7,148 @@ import static org.junit.Assert.assertEquals;
  * This is the AnimationModelTest class. It tests the AnimationModel interface.
  */
 public class AnimationModelTest {
+
   private IAnimationModel testAnimation;
 
+  // IShapes: Rectangle named R & Oval named C
   IShape R;
   IShape C;
 
+  // IEvents for IShape moves
   IEvent move1;
   IEvent move2;
   IEvent move3;
 
+  // IEvents for IShape size changes
   IEvent size1;
 
+  // IEvents for IShape color changes
   IEvent colorChange1;
 
   @Before
   public void setUp() {
-    // create the animation model
+    // Instantiate the animation model
     testAnimation = new AnimationModelImpl();
 
-    // create the shapes
-    R = new Rectangle("R", 50.0, 100.0, 1,0,0,200.0,200.0);
-    C = new Oval("C",120.0, 60.0, 0, 0, 1, 500.0, 100.0);
+    // Instantiate the IShapes
+    R = new Rectangle("R",
+            50.0, 100.0,
+            1,0,0,
+            200.0,200.0);
+
+    C = new Oval("C",
+            120.0, 60.0,
+            0, 0, 1,
+            500.0, 100.0);
   }
 
+  // _________________________________ EXCEPTIONS: Rectangle ______________________________________.
   @Test(expected = IllegalArgumentException.class)
   public void sameShapeName() {
+
     testAnimation.addShape(R, 1, 100);
 
     // new shape with same name
-    IShape F = new Rectangle("R", 2, 4, 3, 50, 10, 30.0, 75.0);
+    IShape F = new Rectangle("R",
+            2, 4,
+            3, 50, 10,
+            30.0, 75.0);
+
     testAnimation.addShape(F, 30, 80);
   }
 
+  // _____________________________________ AddShape() Test ________________________________________.
   @Test
   public void testAddShape() {
-    // add the shapes to the animation
+    // add the "R" & "C" shapes to the animation
     testAnimation.addShape(R, 1, 100);
     testAnimation.addShape(C, 6, 100);
 
-    assertEquals("Shapes:\n"
-                    + "Name: R\n"
-                    + "Type: rectangle\n"
-                    + "Min corner: (200.0,200.0), Width: 50.0, Height: 100.0, Color: (1,0,0)\n"
-                    + "Appears at t=1\n"
-                    + "Disappears at t=100\n"
-                    + "\n"
-                    + "Name: C\n"
-                    + "Type: oval\n"
-                    + "Center: (500.0,100.0), X radius: 60.0, Y radius: 30.0, Color: (0,0,1)\n"
-                    + "Appears at t=6\n"
-            + "Disappears at t=100\n\n", testAnimation.toString());
+    assertEquals("""
+            Shapes:
+            Name: R
+            Type: rectangle
+            Min corner: (200.0,200.0), Width: 50.0, Height: 100.0, Color: (1,0,0)
+            Appears at t=1
+            Disappears at t=100
+
+            Name: C
+            Type: oval
+            Center: (500.0,100.0), X radius: 60.0, Y radius: 30.0, Color: (0,0,1)
+            Appears at t=6
+            Disappears at t=100
+
+            """, testAnimation.toString());
   }
 
+  // ______________________________________ AddMove() Test ________________________________________.
   @Test
   public void testAddMove() {
 
-    // create the moves
-    move1 = new MoveShape(R, 300.0, 300.0);
-    move2 = new MoveShape(C, 500.0, 400.0);
-    move3 = new MoveShape(R, 200.0, 200.0);
+    // Instantiate the moves
+    move1 = new MoveShape(R, 200.0, 200.0, 300.0, 300.0);
+    move2 = new MoveShape(C, 500.0, 100.0, 500.0, 400.0);
+    move3 = new MoveShape(R, 300.0, 300.0, 200.0, 200.0);
 
     testAnimation.addEvent(R, move1, 10, 50);
     testAnimation.addEvent(C, move2, 20, 70);
     testAnimation.addEvent(R, move3, 70, 100);
 
-    assertEquals("Shapes:\n"
-                    + "Shape R moves from (200.0,200.0) to (300.0,300.0) from t=10 to t=50\n"
-                    + "Shape C moves from (500.0,100.0) to (500.0,400.0) from t=20 to t=70\n"
-                    + "Shape R moves from (300.0,300.0) to (200.0,200.0) from t=70 to t=100\n",
+    assertEquals("""
+                    Shapes:
+                    Shape R moves from (200.0,200.0) to (300.0,300.0) from t=10 to t=50
+                    Shape C moves from (500.0,100.0) to (500.0,400.0) from t=20 to t=70
+                    Shape R moves from (300.0,300.0) to (200.0,200.0) from t=70 to t=100
+                    """,
             testAnimation.toString());
   }
 
+  // __________________________________ AddSizeChange() Test ______________________________________.
   @Test
   public void testAddSizeChange() {
-    // create the size change
-    size1 = new ScaleShape(R, 25.0, 100.0);
+
+    // Instantiate the size change
+    size1 = new ScaleShape(R, 50.0, 100.0, 25.0, 100.0);
 
     testAnimation.addEvent(R, size1, 51, 70);
 
-    assertEquals("Shapes:\n" +
-            "Shape R scales from Width: 50.0, Height: 100.0 to Width: 25.0, Height: 100.0 from "
-            + "t=51 to t=70\n", testAnimation.toString());
+    assertEquals("""
+            Shapes:
+            Shape R scales from Width: 50.0, Height: 100.0 to Width: 25.0, Height: 100.0 from t=51 to t=70
+            """, testAnimation.toString());
   }
 
+  // ____________________________________ ColorChange() Test ______________________________________.
   @Test
   public void testColorChange() {
 
-    // create the color change
-    colorChange1 = new ChangeColor(C, 0, 1, 0);
+    // Instantiate the color change
+    colorChange1 = new ChangeColor(C,
+            0, 0, 1,
+            0, 1, 0);
 
     testAnimation.addEvent(C, colorChange1, 50, 80);
 
-    assertEquals("Shapes:\n"
-            + "Shape C changes color from (0,0,1) to (0,1,0) from t=50 to t=80\n",
+    assertEquals("""
+                    Shapes:
+                    Shape C changes color from (0,0,1) to (0,1,0) from t=50 to t=80
+                    """,
             testAnimation.toString());
   }
 
+  // _____________________________________ Test Everything ________________________________________.
   @Test
   public void testEverything() {
     testAnimation.addShape(R, 1, 100);
     testAnimation.addShape(C, 6, 100);
 
-    move1 = new MoveShape(R, 300.0, 300.0);
-    move2 = new MoveShape(C, 500.0, 400.0);
-    move3 = new MoveShape(R, 200.0, 200.0);
+    move1 = new MoveShape(R, 200.0, 200.0, 300.0, 300.0);
+    move2 = new MoveShape(C, 500.0, 100.0, 500.0, 400.0);
+    move3 = new MoveShape(R, 300.0, 300.0, 200.0, 200.0);
 
-    size1 = new ScaleShape(R, 25.0, 100.0);
+    size1 = new ScaleShape(R, 50.0, 100.0, 25.0, 100.0);
 
-    colorChange1 = new ChangeColor(C, 0, 1, 0);
+    colorChange1 = new ChangeColor(C, 0, 0, 1, 0, 1, 0);
 
     testAnimation.addEvent(R, move1, 10, 50);
     testAnimation.addEvent(C, move2, 20, 70);
@@ -123,28 +156,26 @@ public class AnimationModelTest {
     testAnimation.addEvent(R, size1, 51, 70);
     testAnimation.addEvent(R, move3, 70, 100);
 
-    assertEquals("Shapes:\n" +
-                    "Name: R\n" +
-                    "Type: rectangle\n" +
-                    "Min corner: (200.0,200.0), Width: 50.0, Height: 100.0, " +
-                    "Color: (1,0,0)\n" +
-                    "Appears at t=1\n" +
-                    "Disappears at t=100\n" +
-                    "\n" +
-                    "Name: C\n" +
-                    "Type: oval\n" +
-                    "Center: (500.0,100.0), X radius: 60.0, Y radius: 30.0, " +
-                    "Color: (0,0,1)\n" +
-                    "Appears at t=6\n" +
-                    "Disappears at t=100\n" +
-                    "\n" +
-                    "Shape R moves from (200.0,200.0) to (300.0,300.0) from t=10 to t=50\n" +
-                    "Shape C moves from (500.0,100.0) to (500.0,400.0) from t=20 to t=70\n" +
-                    "Shape C changes color from (0,0,1) to (0,1,0) " +
-                    "from t=50 to t=80\n" +
-                    "Shape R scales from Width: 50.0, Height: 100.0 to Width: 25.0, " +
-                    "Height: 100.0 from t=51 to t=70\n" +
-                    "Shape R moves from (300.0,300.0) to (200.0,200.0) from t=70 to t=100\n"
+    assertEquals("""
+                    Shapes:
+                    Name: R
+                    Type: rectangle
+                    Min corner: (200.0,200.0), Width: 50.0, Height: 100.0, Color: (1,0,0)
+                    Appears at t=1
+                    Disappears at t=100
+
+                    Name: C
+                    Type: oval
+                    Center: (500.0,100.0), X radius: 60.0, Y radius: 30.0, Color: (0,0,1)
+                    Appears at t=6
+                    Disappears at t=100
+
+                    Shape R moves from (200.0,200.0) to (300.0,300.0) from t=10 to t=50
+                    Shape C moves from (500.0,100.0) to (500.0,400.0) from t=20 to t=70
+                    Shape C changes color from (0,0,1) to (0,1,0) from t=50 to t=80
+                    Shape R scales from Width: 50.0, Height: 100.0 to Width: 25.0, Height: 100.0 from t=51 to t=70
+                    Shape R moves from (300.0,300.0) to (200.0,200.0) from t=70 to t=100
+                    """
             , testAnimation.toString());
   }
 }
--- test/EventTest.java
+++ test/EventTest.java
@@ -23,83 +23,183 @@ public class EventTest {
   @Before
   public void setUp() {
     // make some shapes
-    this.oval = new Oval("oval1", 120, 60, 0,
-            0, 1, 500.0, 100.0);
-    this.rect = new Rectangle("rect1", 50, 100, 1,
-            0, 0, 200.0, 200.0);
     // make some events to test their getters and setters
-    this.colorEvent = new ChangeColor(oval, 0, 1, 0);
-    this.scaleEvent = new ScaleShape(oval, 60.0, 30.0);
-    this.moveEvent = new MoveShape(rect, 50.0, 100.0);
+
+    //________________________________________ Oval Objects _______________________________________.
+
+    this.oval = new Oval("oval1",
+            120, 60,
+            0, 0, 1,
+            500.0, 100.0);
+
+    //_____________________________________ Rectangle Objects _____________________________________.
+
+    this.rect = new Rectangle("rect1",
+            50, 100,
+            1, 0, 0,
+            200.0, 200.0);
+
+    //________________________________________ Oval Events ________________________________________.
+
+    this.colorEvent = new ChangeColor(oval,
+            0, 0, 1,
+            100, 25, 3);
+
+    this.scaleEvent = new ScaleShape(oval,
+            120.0, 60.0,
+            10.1, 30.9);
+
+    //_____________________________________ Rectangle Events ______________________________________.
+
+    this.moveEvent = new MoveShape(rect,
+            200.0, 200.0,
+            50.0, 100.0);
   }
 
   @Test
   public void testValidConstructor() {
     // test normal examples
-    //this.colorEvent = new ChangeColor(oval, 100, 25, 3);
-    //assertEquals("Shape oval1 changes color from (0,0,1) to (100,25,3) from t=0 to t=0",
-      //      colorEvent.toString());
-    this.scaleEvent = new ScaleShape(oval, 10.09, 30.90123);
+    // test larger numbers and with rectangle
+
+    //________________________________________ Oval Events ________________________________________.
+    this.colorEvent = new ChangeColor(oval,
+            0, 0, 1,
+            100, 25, 3);
+
+    assertEquals("Shape oval1 changes color from (0,0,1) to (100,25,3) from t=0 to t=0",
+            colorEvent.toString());
+
+    this.scaleEvent = new ScaleShape(oval,
+            120.0, 60.0,
+            10.1, 30.90123);
+
     assertEquals("Shape oval1 scales from Width: 120.0, Height: 60.0 to Width: 10.1, "
-            + "Height: 30.9 from t=0 to t=0", scaleEvent.toString());
-    this.moveEvent = new MoveShape(oval, 14.5, 78.432);
+            + "Height: 30.9 from t=0 to t=0",
+            scaleEvent.toString());
+
+    this.moveEvent = new MoveShape(oval,
+            500.0, 100.0,
+            14.5, 78.432);
+
     assertEquals("Shape oval1 moves from (500.0,100.0) to (14.5,78.4) from t=0 to t=0",
             moveEvent.toString());
-    // test larger numbers and with rectangle
-    IEvent colorCheck = new ChangeColor(rect, 255, 255, 255);
+
+    //_____________________________________ Rectangle Events ______________________________________.
+    IEvent colorCheck = new ChangeColor(rect,
+            1, 0, 0,
+            255, 255, 255);
+
     assertEquals("Shape rect1 changes color from (1,0,0) to (255,255,255) from t=0 to t=0",
             colorCheck.toString());
-    IEvent scaleCheck = new ScaleShape(rect, 1784357921834.28349, 9872345.8300);
+
+    IEvent scaleCheck = new ScaleShape(rect,
+            50.0, 100.0,
+            1784357921834.28349, 9872345.8300);
+
     assertEquals("Shape rect1 scales from Width: 50.0, Height: 100.0 to "
-            + "Width: 1784357921834.3, Height: 9872345.8 from t=0 to t=0", scaleCheck.toString());
-    IEvent moveCheck = new MoveShape(rect, 754732847528.00042, 8247582);
+            + "Width: 1784357921834.3, Height: 9872345.8 from t=0 to t=0",
+            scaleCheck.toString());
+
+    IEvent moveCheck = new MoveShape(rect,
+            200.0, 200.0,
+            754732847528.00042, 8247582);
+
     assertEquals("Shape rect1 moves from (200.0,200.0) to "
-            + "(754732847528.0,8247582.0) from t=0 to t=0", moveCheck.toString());
-    // negative move
-    IEvent negMoveCheck = new MoveShape(rect, -234753489.346, -2);
+            + "(754732847528.0,8247582.0) from t=0 to t=0",
+            moveCheck.toString());
+
+    // ____________________ Negative Move ____________________
+    IEvent negMoveCheck = new MoveShape(rect,
+            200.0, 200.0,
+            -234753489.346, -2);
+
     assertEquals("Shape rect1 moves from (200.0,200.0) to "
-            + "(-234753489.3,-2.0) from t=0 to t=0", negMoveCheck.toString());
-    // test with zeros
-    IEvent zeroColorCheck = new ChangeColor(rect, 0, 0, 0);
+            + "(-234753489.3,-2.0) from t=0 to t=0",
+            negMoveCheck.toString());
+
+    // ____________________ Test With Zeros ____________________
+    IEvent zeroColorCheck = new ChangeColor(rect,
+            1, 0, 0,
+            0,0,0);
+
     assertEquals("Shape rect1 changes color from (1,0,0) to (0,0,0) from t=0 to t=0",
             zeroColorCheck.toString());
-    IEvent zeroMoveCheck = new MoveShape(rect, 0, 0);
+
+    IEvent zeroMoveCheck = new MoveShape(rect,
+            200.0, 200.0,
+            0, 0);
     assertEquals("Shape rect1 moves from (200.0,200.0) to "
             + "(0.0,0.0) from t=0 to t=0", zeroMoveCheck.toString());
   }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void testZeroWidth() {
-    new ScaleShape(rect, 0, 5);
-  }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void testZeroHeight() {
-    new ScaleShape(rect, 10, 0);
-  }
+  // ____________________________________ EXCEPTIONS: Oval ________________________________________.
 
-  @Test(expected = IllegalArgumentException.class)
-  public void testNegativeWidth() {
-    new ScaleShape(rect, -192, 5);
-  }
-
-  @Test(expected = IllegalArgumentException.class)
-  public void testNegativeHeight() {
-    new ScaleShape(rect, 10, -1.3);
-  }
-
+  // Original Oval RGB: 0, 0, 1
+  // New Oval RGB: -5, 200, 200
   @Test(expected = IllegalArgumentException.class)
   public void testNegativeRed() {
-    new ChangeColor(oval, -5, 200, 200);
+    new ChangeColor(oval,
+            0, 0, 1,
+            -5, 200, 200);
   }
+
+  // Original Oval RGB: 0, 0, 1
+  // New Oval RGB: 200, -200, 200
   @Test(expected = IllegalArgumentException.class)
   public void testNegativeGreen() {
-    new ChangeColor(oval, 200, -200, 200);
+    new ChangeColor(oval,
+            0, 0, 1,
+            200, -200, 200);
   }
+
+  // Original Oval RGB: 0, 0, 1
+  // New Oval RGB: 200, 200, 200
   @Test(expected = IllegalArgumentException.class)
   public void testNegativeBlue() {
-    new ChangeColor(oval, 200, 200, 200);
+    new ChangeColor(oval,
+            0, 0, 1,
+            200, 200, 200);
   }
+
+  // _________________________________ EXCEPTIONS: Rectangle ______________________________________.
+
+  // Original Rectangle Width, Height: 50.0, 100.0
+  // New Rectangle Width, Height: 0, 5
+  @Test(expected = IllegalArgumentException.class)
+  public void testZeroWidth() {
+    new ScaleShape(rect,
+            50.0, 100.0,
+            0, 5);
+  }
+
+  // Original Rectangle Width, Height: 50.0, 100.0
+  // New Rectangle Width, Height: 10, 0
+  @Test(expected = IllegalArgumentException.class)
+  public void testZeroHeight() {
+    new ScaleShape(rect,
+            50.0, 100.0,
+            10, 0);
+  }
+
+  // Original Rectangle Width, Height: 50.0, 100.0
+  // New Rectangle Width, Height: -192, 5
+  @Test(expected = IllegalArgumentException.class)
+  public void testNegativeWidth() {
+    new ScaleShape(rect,
+            50.0, 100.0,
+            -192, 5);
+  }
+
+  // Original Rectangle Width, Height: 50.0, 100.0
+  // New Rectangle Width, Height: 10, -1.3
+  @Test(expected = IllegalArgumentException.class)
+  public void testNegativeHeight() {
+    new ScaleShape(rect,
+            50.0, 100.0,
+            10, -1.3);
+  }
+
 
 }
 
