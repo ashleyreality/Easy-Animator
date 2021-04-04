40658e0ff6d42003a835f48e323501da15b7c096
aya :) <ayagare@gmail.com>
2021-04-03 20:13:52 -0400

    Updated some JavaDocs for IShape interface.

--- src/IShape.java
+++ src/IShape.java
@@ -1,103 +1,114 @@
 /**
- * This is the Shape interface. It declares the methods all types of shapes should support.
+ * ___________________________________ INTERFACE: IShape {} _______________________________________.
+ * The Shape interface declares the methods all types of shapes should support.
  */
 public interface IShape extends Comparable<IShape> {
 
+  /**
+   * ____________________________________ METHOD: getName() _______________________________________.
+   *
+   * @return the unique name of the shape
+   */
   String getName();
 
+  /**
+   * ____________________________________ METHOD: setName() _______________________________________.
+   *
+   * @param name - the unique name given to the shape
+   */
   void setName(String name);
 
   /**
    * ___________________________________ METHOD: getColor() _______________________________________.
-   * The getColor() method outputs the current color of a given shape.
    *
-   * @return a Color associated with the given shape
+   * @return the color associated with the given shape
    */
   Color getColor();
 
+  /**
+   * ___________________________________ METHOD: setColor() _______________________________________.
+   *
+   * @param red   - the integer value of the red color, an int
+   * @param green - the integer value of the green color, an int
+   * @param blue  - the integer value of the blue color, an int
+   */
   void setColor(int red, int green, int blue);
 
   /**
    * __________________________________ METHOD: getLocation() _____________________________________.
-   * The getLocation() method outputs the current location of a given shape.
    *
-   * @return a Point2D location of the given shape
+   * @return the Point2D location of the given shape
    */
   Point2D getLocation();
 
   /**
-   * Sets an object's location.
+   * __________________________________ METHOD: getLocation() _____________________________________.
    *
-   * @param x - the x value of the location
-   * @param y - the y value of the location
+   * @param x - the x value of the shape's location, a double
+   * @param y - the y value of the shape's location, a double
    */
   void setLocation(double x, double y);
 
   /**
    * ___________________________________ METHOD: getAppear() ______________________________________.
-   * The getAppear() method outputs the time/tick of when a given shape appears.
    *
-   * @return the tick value of when the given shape appears, an int
+   * @return the tick/time value of when the given shape appears, an int
    */
   int getAppear();
 
   /**
-   * Sets the time a Shape appears.
+   * ___________________________________ METHOD: setAppear() ______________________________________.
    *
-   * @param appear - the time in ticks when the Shape appears
+   * @param appear - the time in ticks when the Shape is set to appear, an int
    */
   void setAppear(int appear);
 
   /**
    * _________________________________ METHOD: getDisappear() _____________________________________.
-   * The getDisappear() method outputs the time/tick of when a given shape disappears.
    *
    * @return the tick value of when the given shape disappears, an int
    */
   int getDisappear();
 
   /**
-   * Sets the time a Shape disappears.
+   * ___________________________________ METHOD: setDisappear() ______________________________________.
    *
-   * @param disappear - the time in ticks when a Shape disappears
+   * @param disappear - the time in ticks when the Shape is set to disappear, an int
    */
   void setDisappear(int disappear);
 
   /**
    * ____________________________________ METHOD: getWidth() ______________________________________.
-   * The getWidth() method outputs the width of a given shape. The width is the horizontal length of
-   * a rectangle and the horizontal diameter of an oval.
+   * The width is the horizontal length of a rectangle and the horizontal diameter of an oval.
    *
    * @return the width value of the given shape, a double
    */
   double getWidth();
 
   /**
-   * Sets the width of a given shape. The width is the horizontal length of a rectangle and the
-   * horizontal diameter of an oval.
+   * ____________________________________ METHOD: setWidth() ______________________________________.
+   * The width is the horizontal length of a rectangle and the horizontal diameter of an oval.
    *
-   * @param width - a double, the width of a Shape
-   * @throws IllegalArgumentException if width is negative
+   * @param width - the width the Shape is being set to, a double
+   * @throws IllegalArgumentException if the width being passed in is negative
    */
   void setWidth(double width);
 
   /**
    * ____________________________________ METHOD: getHeight() _____________________________________.
-   * The getHeight() method outputs the height of a given shape. The height is the vertical length
-   * of a rectangle, or the vertical length of an oval.
+   * The height is the vertical length of a rectangle, or the vertical length of an oval.
    *
    * @return the height value of the given shape, a double
    */
   double getHeight();
 
   /**
-   * Sets the height of a given shape. The height is the vertical length of a rectangle, or the
-   * vertical length of an oval.
+   * ____________________________________ METHOD: setHeight() _____________________________________.
+   * The height is the vertical length of a rectangle, or the vertical length of an oval.
    *
-   * @param height - the height value of the given shape, a double
-   * @throws IllegalArgumentException if height is negative
+   * @param height - the height the Shape is being set to, a double
+   * @throws IllegalArgumentException if the height being passed in is negative
    */
   void setHeight(double height);
 
-//  IShape copy();
 }
