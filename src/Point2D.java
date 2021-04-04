3e7aa907e7c620827b838a2816efea027afe37fc
aya :) <ayagare@gmail.com>
2021-04-03 22:25:34 -0400

    Updated some JavaDocs for Point2D class. Added exception for negative coordinates & commented it out for now.

--- src/Point2D.java
+++ src/Point2D.java
@@ -1,4 +1,5 @@
 /**
+ * ________________________________ CONCRETE CLASS: Point2D _______________________________________.
  * Prof. Park's Point2D class. This class represents a 2D point. This point is denoted in Cartesian
  * coordinates as (x,y).
  */
@@ -7,18 +8,25 @@ public class Point2D {
   private double y;
 
   /**
-   * Construct a 2d point with the given coordinates
+   * _________________________________ CONSTRUCTOR: Point2D() _____________________________________.
+   * Constructs a 2d point with the given coordinates.
    *
    * @param x the x-coordinate of this point
    * @param y the y-coordinate of this point
    */
   public Point2D(double x, double y) {
+
+    //    if (x < 0 || y < 0) {
+    //      throw new IllegalArgumentException("The x and y coordinates can not be negative values.");
+    //    }
+
     this.x = x;
     this.y = y;
   }
 
   /**
-   * Return the x-coordinate of this point
+   * ___________________________________ METHOD: getX() ___________________________________________.
+   * Outputs the x-coordinate of this point.
    *
    * @return x-coordinate of this point
    */
@@ -27,7 +35,8 @@ public class Point2D {
   }
 
   /**
-   * Return the y-coordinate of this point
+   * ___________________________________ METHOD: getY() ___________________________________________.
+   * Outputs the y-coordinate of this point.
    *
    * @return y-coordinate of this point
    */
@@ -35,6 +44,12 @@ public class Point2D {
     return y;
   }
 
+  /**
+   * ___________________________________ METHOD: toString() _______________________________________.
+   * This is the toString() method override. It outputs a String with the location of the shape.
+   *
+   * @return a String of the shape's location
+   */
   public String toString() {
     return String.format("(%.1f,%.1f)", this.x, this.y);
   }
