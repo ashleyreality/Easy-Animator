c1590eb3528f1841f902ec58153d5d001dabacdb
ashley <51171130+ashleyreality@users.noreply.github.com>
2021-04-04 13:44:54 -0400

    minor documentation tweaks

--- src/IShape.java
+++ src/IShape.java
@@ -6,6 +6,7 @@ public interface IShape extends Comparable<IShape> {
 
   /**
    * ____________________________________ METHOD: getName() _______________________________________.
+   * Returns the shape name.
    *
    * @return the unique name of the shape
    */
@@ -13,6 +14,7 @@ public interface IShape extends Comparable<IShape> {
 
   /**
    * ____________________________________ METHOD: setName() _______________________________________.
+   * Sets the shape name.
    *
    * @param name - the unique name given to the shape
    */
@@ -20,6 +22,7 @@ public interface IShape extends Comparable<IShape> {
 
   /**
    * ___________________________________ METHOD: getColor() _______________________________________.
+   * Gets the shape's color.
    *
    * @return the color associated with the given shape
    */
@@ -27,6 +30,7 @@ public interface IShape extends Comparable<IShape> {
 
   /**
    * ___________________________________ METHOD: setColor() _______________________________________.
+   * Sets the shape's color.
    *
    * @param red   - the integer value of the red color, an int
    * @param green - the integer value of the green color, an int
@@ -36,13 +40,15 @@ public interface IShape extends Comparable<IShape> {
 
   /**
    * __________________________________ METHOD: getLocation() _____________________________________.
+   * Gets the shape's location.
    *
    * @return the Point2D location of the given shape
    */
   Point2D getLocation();
 
   /**
-   * __________________________________ METHOD: getLocation() _____________________________________.
+   * __________________________________ METHOD: setLocation() _____________________________________.
+   * Sets the shape's location.
    *
    * @param x - the x value of the shape's location, a double
    * @param y - the y value of the shape's location, a double
@@ -51,6 +57,7 @@ public interface IShape extends Comparable<IShape> {
 
   /**
    * ___________________________________ METHOD: getAppear() ______________________________________.
+   * Gets the shape's appear time.
    *
    * @return the tick/time value of when the given shape appears, an int
    */
@@ -58,6 +65,7 @@ public interface IShape extends Comparable<IShape> {
 
   /**
    * ___________________________________ METHOD: setAppear() ______________________________________.
+   * Sets the shape's appear time.
    *
    * @param appear - the time in ticks when the Shape is set to appear, an int
    */
@@ -65,6 +73,7 @@ public interface IShape extends Comparable<IShape> {
 
   /**
    * _________________________________ METHOD: getDisappear() _____________________________________.
+   * Gets the shape's disappear time.
    *
    * @return the tick value of when the given shape disappears, an int
    */
@@ -72,6 +81,7 @@ public interface IShape extends Comparable<IShape> {
 
   /**
    * ___________________________________ METHOD: setDisappear() __________________________________.
+   * Sets the shape's disappear time.
    *
    * @param disappear - the time in ticks when the Shape is set to disappear, an int
    */
@@ -79,7 +89,8 @@ public interface IShape extends Comparable<IShape> {
 
   /**
    * ____________________________________ METHOD: getWidth() ______________________________________.
-   * The width is the horizontal length of a rectangle and the horizontal diameter of an oval.
+   * Gets the shape's width. The width is the horizontal length of a rectangle and the horizontal
+   * diameter of an oval.
    *
    * @return the width value of the given shape, a double
    */
@@ -87,7 +98,8 @@ public interface IShape extends Comparable<IShape> {
 
   /**
    * ____________________________________ METHOD: setWidth() ______________________________________.
-   * The width is the horizontal length of a rectangle and the horizontal diameter of an oval.
+   * Sets the shape's width. The width is the horizontal length of a rectangle and the horizontal
+   * diameter of an oval.
    *
    * @param width - the width the Shape is being set to, a double
    * @throws IllegalArgumentException if the width being passed in is negative
@@ -96,7 +108,8 @@ public interface IShape extends Comparable<IShape> {
 
   /**
    * ____________________________________ METHOD: getHeight() _____________________________________.
-   * The height is the vertical length of a rectangle, or the vertical length of an oval.
+   * Gets the shape's height. The height is the vertical length of a rectangle, or the vertical
+   * length of an oval.
    *
    * @return the height value of the given shape, a double
    */
@@ -104,7 +117,8 @@ public interface IShape extends Comparable<IShape> {
 
   /**
    * ____________________________________ METHOD: setHeight() _____________________________________.
-   * The height is the vertical length of a rectangle, or the vertical length of an oval.
+   * Sets the shape's height. The height is the vertical length of a rectangle, or the vertical
+   * length of an oval.
    *
    * @param height - the height the Shape is being set to, a double
    * @throws IllegalArgumentException if the height being passed in is negative
