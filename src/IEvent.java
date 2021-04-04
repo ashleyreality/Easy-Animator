44b9c86e1f8d160a7db98d199aeb81316578d154
aya :) <ayagare@gmail.com>
2021-04-03 20:01:02 -0400

    Updated some JavaDocs for IEvent interface.

--- src/IEvent.java
+++ src/IEvent.java
@@ -1,21 +1,48 @@
 /**
+ * ___________________________________ INTERFACE: IEvent {} _______________________________________.
  * The IEvent interface creates a shape change event.
  */
 public interface IEvent {
 
+  /**
+   * _________________________________ METHOD: setEventBegin() ____________________________________.
+   * Sets the start time of the shape change.
+   *
+   * @param begin the start time of the change
+   * @throws IllegalArgumentException if start tick is negative
+   */
   public void setEventBegin(int begin);
 
+  /**
+   * ___________________________________ METHOD: setEventEnd() ____________________________________.
+   * Sets the end time of the shape change.
+   *
+   * @param end the end time of the change
+   * @throws IllegalArgumentException if end tick is negative or less than begin
+   */
   public void setEventEnd(int end);
 
+  /**
+   * _________________________________ METHOD: getEventBegin() ____________________________________.
+   * Get the start time of the shape change.
+   *
+   * @return the time for when the change started
+   */
   public int getEventBegin();
 
+  /**
+   * _________________________________ METHOD: getEventEnd() ______________________________________.
+   * Get the end time of the shape change.
+   *
+   * @return the time for when the change stopped
+   */
   public int getEventEnd();
 
-  /**
-   * The change method creates a changed shape event.
-   *
-   * @param shape - the shape you want to change
-   * @throws IllegalArgumentException if the shape does not exist
-   */
-  public void change(IShape shape) throws IllegalArgumentException;
+  //  /**
+  //   * The change method creates a changed shape event.
+  //   *
+  //   * @param shape - the shape you want to change
+  //   * @throws IllegalArgumentException if the shape does not exist
+  //   */
+  //public void change(IShape shape) throws IllegalArgumentException;
 }
