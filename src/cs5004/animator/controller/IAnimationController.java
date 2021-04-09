7a6135608a8f791656b3922bdf02ccd744fa9e57
ashley <51171130+ashleyreality@users.noreply.github.com>
2021-04-09 11:06:41 -0400

    altho maybe we don't need them?

--- src/cs5004/animator/controller/IAnimationController.java
+++ src/cs5004/animator/controller/IAnimationController.java
@@ -1,7 +1,8 @@
 package cs5004.animator.controller;
 
 import cs5004.animator.model.IAnimationModel;
-
+// I made this, but we might not need it -- looking at my notes I think we are supposed to use the
+// main method for this. Whoops --AB
 /**
  * Represents a Controller for a simple animation. Executes shapes and events using the model;
  * conveys that information to the views.
