9fe0144349f859e86698eb9ab0fcf3e5294c5481
aya :) <ayagare@gmail.com>
2021-04-11 18:39:32 -0400

    Updated JavaDocs in AnimatorHelper and EasyAnimator.

--- src/cs5004/animator/AnimatorHelper.java
+++ src/cs5004/animator/AnimatorHelper.java
@@ -14,26 +14,34 @@ import cs5004.animator.util.Builder;
 
 public class AnimatorHelper {
   private static IAnimationModel model;
-  //  private Readable in;
-  //  private Appendable out;
 
   /**
-   * _____________________________ CONSTRUCTOR: AnimatorHelper() __________________________________.
+   * ______________________________ HELPER METHOD: modelSetup() ___________________________________.
+   * @return
    */
-//  public AnimatorHelper(IAnimationModel model) {
-//    AnimatorHelper.model = model;
-//  }
-
   public static IAnimationModel modelSetup() {
     model = new AnimationModelImpl();
     return model;
   }
 
+  /**
+   * ______________________________ HELPER METHOD: buildSetup() ___________________________________.
+   * The AnimationBuilder takes in the model which is then cast from an AnimationReader type to an
+   * IAnimationModel type and is then parsed using the input file using the instance of
+   * AnimationBuilder().
+   * @return the build, an AnimationBuilder
+   */
   public static AnimationBuilder buildSetup() {
     AnimationBuilder build = new Builder(model);
     return build;
   }
 
+  /**
+   * _____________________________ HELPER METHOD: buildToModel() __________________________________.
+   * @param file the input file, a Readable
+   * @param build the build with the shapes and their events, an AnimationBuilder
+   * @return the model, an IAnimationModel
+   */
   public static IAnimationModel buildToModel(Readable file, AnimationBuilder build) {
     model = (IAnimationModel) AnimationReader.parseFile(file, build);
     return model;
@@ -41,7 +49,7 @@ public class AnimatorHelper {
 
   /**
    * _____________________________ HELPER METHOD: jframeStart() ___________________________________.
-   * @return
+   * @return the frame of the model, a JFrame
    */
   public static JFrame jframeStart() {
     JFrame frame = new JFrame("Frame");
@@ -49,6 +57,13 @@ public class AnimatorHelper {
     return frame;
   }
 
+  /**
+   * _____________________________ HELPER METHOD: stringBuilder() _________________________________.
+   * Convert the input to main "args" from an array of Strings to tokens of Strings with a space in
+   * between each token.
+   * @param args the array of Strings
+   * @return the string builder, a StringBuilder
+   */
   public static StringBuilder stringBuilder(String[] args) {
     StringBuilder sb = new StringBuilder();
     for (String arg : args) {
@@ -58,17 +73,38 @@ public class AnimatorHelper {
     return sb;
   }
 
+  /**
+   * _____________________________ HELPER METHOD: nameScanner() ___________________________________.
+   * The Scanner reads through the Strings of tokens in the string builder and looks for the String
+   * "-in" and assigns the String right after it to variable "inResult" which represents the name of
+   * the .txt input file.
+   * @param sb the string builder, a StringBuilder
+   * @return the next String after "-in", a String
+   */
   public static String nameScanner(StringBuilder sb) {
     Scanner in = new Scanner(sb.toString());
     in.findInLine("-in");
     return in.next();
   }
 
+  /**
+   * _______________________________ HELPER METHOD: setFile() _____________________________________.
+   * The FileReader takes in the input file.
+   * @param inputName the name of the input file, a String
+   * @return the file, a Readable
+   * @throws FileNotFoundException if the file is not found
+   */
   public static Readable setFile(String inputName) throws FileNotFoundException {
     Readable file = new FileReader(inputName);
     return file;
   }
 
+  /**
+   * _____________________________ HELPER METHOD: fileExceptions() ________________________________.
+   * @param file the input file, a Readable
+   * @param inputName the name of the input file, a String
+   * @param frame the frame of the model, a JFrame
+   */
   public static void fileExceptions(Readable file, String inputName, JFrame frame) {
     try {
       file = new FileReader(inputName);
@@ -82,12 +118,26 @@ public class AnimatorHelper {
     }
   }
 
+  /**
+   * _____________________________ HELPER METHOD: viewScanner() ___________________________________.
+   * The Scanner reads through the Strings of tokens in the string builder and looks for the String
+   * "-view" and assigns the String right after it to variable "outputView" which represents the
+   * view type of the output.
+   * @param sb the string builder, a StringBuilder
+   * @return the next String after "-view", a String
+   */
   public static String viewScanner(StringBuilder sb) {
     Scanner view = new Scanner(sb.toString());
     view.findInLine("-view");
     return view.next();
   }
 
+  /**
+   * ___________________________ HELPER METHOD: viewExceptions() __________________________________.
+   * If the view type is wrong, pop up an error message.
+   * @param outputView the view type being output
+   * @param frame the frame of the view, a JFrame
+   */
   public static void viewExceptions(String outputView, JFrame frame) {
     if (!outputView.equalsIgnoreCase("text")
             && !outputView.equalsIgnoreCase("svg")
@@ -99,18 +149,40 @@ public class AnimatorHelper {
     }
   }
 
+  /**
+   * ______________________________ HELPER METHOD: outScanner() ___________________________________.
+   * The Scanner reads through the Strings of tokens in the string builder and looks for the String
+   * "-out" and assigns the String right after it to variable "outputName" which represents the name
+   * of the output (any type).
+   * @param sb the string builder, a StringBuilder
+   * @return the next String after "-out", a String
+   */
   public static String outScanner(StringBuilder sb) {
     Scanner out = new Scanner(sb.toString());
     out.findInLine("-out");
     return out.next();
   }
 
+  /**
+   * _____________________________ HELPER METHOD: speedScanner() __________________________________.
+   * The Scanner reads through the Strings of tokens in the string builder and looks for the String
+   * "-speed" and assigns the String right after it to variable "outputSpeed" which represents the
+   * outputSpeed within the output (any type).
+   * @param sb the string builder, a StringBuilder
+   * @return the next String after "-speed", a String
+   */
   public static String speedScanner(StringBuilder sb) {
     Scanner outputSpeed = new Scanner(sb.toString());
     outputSpeed.findInLine("-speed");
     return outputSpeed.next();
   }
 
+  /**
+   * ___________________________ HELPER METHOD: speedExceptions() _________________________________.
+   * If the outputSpeed is less than the integer value of 1, pop up an error message.
+   * @param outputSpeed the output speed of the animation, a String
+   * @param frame the frame of the view, a JFrame
+   */
   public static void speedExceptions(String outputSpeed, JFrame frame) {
     try {
       int speedInt = Integer.parseInt(outputSpeed);
--- src/cs5004/animator/EasyAnimator.java
+++ src/cs5004/animator/EasyAnimator.java
@@ -4,11 +4,8 @@ import java.io.FileNotFoundException;
 
 import javax.swing.JFrame;
 
-import cs5004.animator.model.AnimationModelImpl;
 import cs5004.animator.model.IAnimationModel;
 import cs5004.animator.util.AnimationBuilder;
-import cs5004.animator.util.AnimationReader;
-import cs5004.animator.util.Builder;
 import cs5004.animator.util.ViewFactory;
 
 
@@ -17,103 +14,68 @@ public class EasyAnimator {
 
   public static void main(String[] args) throws FileNotFoundException {
 
+    // fixme - what to do if outputSpeed isn't given and also isn't needed, like for text view?
+    // only throw an error if it's required for the given view. if outputSpeed is req'd and not given,
+    // it's ok to ignore. If you need the outputSpeed and don't get it, you can either default to a given
+    // outputSpeed or you can throw an error. Defaulting preferred -- be sure to document it!
+
     // The controller/main uses a Readable object to
     // read one "token" at a time (where each token is a String separated by a space)
     // from the input (like a text file input)
     // and it decides how/what will be output for the view's use
 
     // Return the Appendable object for the View to use
-    // The View (such as TextView) uses an Appendable object to transmit all outputs (like a text file output)
+    // The View (such as TextView) uses an Appendable object to transmit all outputs (like a text
+    // file output)
 
-    // ----------------------------------------------------------------------------------------
+    // ___________________________________ Invoke modelSetup() ____________________________________.
     // setup the *model* using an instance of AnimationModelImpl()
-    //AnimatorHelper model = new AnimatorHelper();
     IAnimationModel model = AnimatorHelper.modelSetup();
-    //IAnimationModel model = new AnimationModelImpl();
-    //AnimatorHelper helper = new AnimatorHelper(model);
 
-    // ----------------------------------------------------------------------------------------
+    // __________________________________ Invoke jframeStart() ____________________________________.
     // setup the *frame* using an instance of JFrame()
     JFrame frame = AnimatorHelper.jframeStart();
 
-    // ----------------------------------------------------------------------------------------
+    // _________________________________ Invoke stringBuilder() ___________________________________.
     // setup the *string builder* using an instance of StringBuilder()
-    // convert the input to main "args" from an array of Strings to tokens of Strings
-    // with a space in between each token
     StringBuilder sb = AnimatorHelper.stringBuilder(args);
 
-    // ----------------------------------------------------------------------------------------
+    // _________________________________ Invoke nameScanner() _____________________________________.
     // setup the *input* using an instance of Scanner()
-    // the Scanner reads through the Strings of tokens in the string builder
-    // and looks for the String "-in" and assigns the String right after it to variable "inResult"
-    // which represents the name of the .txt input file
     String inputName = AnimatorHelper.nameScanner(sb);
 
-    // ----------------------------------------------------------------------------------------
+    // ___________________________________ Invoke setFile() _______________________________________.
     // setup the *input file reader* using an instance of FileReader()
-    // the FileReader takes in the input file
-    //Readable file = null;
     Readable file = AnimatorHelper.setFile(inputName);
-    //AnimatorHelper.setFile(file, inputName);
     AnimatorHelper.fileExceptions(file, inputName, frame);
 
-    // ----------------------------------------------------------------------------------------
+    // __________________________________ Invoke buildSetup() _____________________________________.
     // setup the *build* using an instance of AnimationBuilder()
-    // the AnimationBuilder takes in the model
-    // which is then cast from an AnimationReader type to an IAnimationModel type
-    // and is then parsed using the input file using the instance of AnimationBuilder()
     AnimationBuilder build = AnimatorHelper.buildSetup();
-    //AnimationBuilder build = new Builder(model);
     AnimatorHelper.buildToModel(file, build);
-    //model = (IAnimationModel) AnimationReader.parseFile(file, build);
 
-    // ----------------------------------------------------------------------------------------
+    // _________________________________ Invoke viewScanner() _____________________________________.
     // setup the *view* using an instance of Scanner()
-    // the Scanner reads through the Strings of tokens in the string builder
-    // and looks for the String "-view" and assigns the String right after it to variable "outputView"
-    // which represents the view type of the output
     String outputView = AnimatorHelper.viewScanner(sb);
-
-    // if the view type is wrong, pop up an error message
     AnimatorHelper.viewExceptions(outputView, frame);
 
-    // ----------------------------------------------------------------------------------------
+    // __________________________________ Invoke outScanner() _____________________________________.
     // setup the *out* using an instance of Scanner()
-    // the Scanner reads through the Strings of tokens in the string builder
-    // and looks for the String "-out" and assigns the String right after it to variable "outputName"
-    // which represents the name of the output (any type)
+
     String outputName = AnimatorHelper.outScanner(sb);
 
-    // fixme - what to do if outputSpeed isn't given and also isn't needed, like for text view?
-    // only throw an error if it's required for the given view. if outputSpeed is req'd and not given,
-    // it's ok to ignore. If you need the outputSpeed and don't get it, you can either default to a given
-    // outputSpeed or you can throw an error. Defaulting preferred -- be sure to document it!
-
-    // ----------------------------------------------------------------------------------------
+    // _________________________________ Invoke speedScanner() ____________________________________.
     // setup the *outputSpeed* using an instance of Scanner()
-    // the Scanner reads through the Strings of tokens in the string builder
-    // and looks for the String "-speed" and assigns the String right after it to variable "outputSpeed"
-    // which represents the outputSpeed within the output (any type)
     String outputSpeed = AnimatorHelper.speedScanner(sb);
-
-    // if the outputSpeed is less than the integer value of 1, pop up an error message
     AnimatorHelper.speedExceptions(outputSpeed, frame);
 
-    // ----------------------------------------------------------------------------------------
+    // _______________________________ Instantiate ViewFactory() __________________________________.
     // setup the *view* using an instance of ViewFactory()
-    // the ViewFactory takes in:
-    // 1) the view type of the output,
-    // 2) the model,
-    // 3) the name of the output,
-    // 4) and the outputSpeed of the output
     ViewFactory newView = new ViewFactory(outputView, model, outputName, outputSpeed);
     newView.create();
 
-    // ----------------------------------------------------------------------------------------
-    // JFrame finishing up
+    // ________________________________ End the JFrame Instance ___________________________________.
     frame.pack();
     System.exit(0);
   }
-
-
 }
