4186507de7ea0cd3824bb9e5b7a68cb5be57528b
ashley <51171130+ashleyreality@users.noreply.github.com>
2021-04-11 16:32:48 -0400

    refactoring SVG with helper methods

--- src/cs5004/animator/view/SVGView.java
+++ src/cs5004/animator/view/SVGView.java
@@ -7,6 +7,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import cs5004.animator.model.EventType;
 import cs5004.animator.model.IAnimationModel;
 import cs5004.animator.model.IEvent;
 import cs5004.animator.model.IShape;
@@ -14,13 +15,14 @@ import cs5004.animator.model.IShape;
 public class SVGView implements IView {
   private PrintWriter file;
   private IAnimationModel model;
+  private StringBuilder sb;
 
   public SVGView(IAnimationModel model, String fileName) throws FileNotFoundException {
     this.file = new PrintWriter(fileName);
     this.model = model;
 
     // create StringBuilder
-    StringBuilder sb = new StringBuilder();
+    sb = new StringBuilder();
 
     // add initial xml to sb
     String str = "<svg width=\"" + model.getBoundsWidth() + "\" height=\"" + model.getBoundsHeight()
@@ -32,33 +34,23 @@ public class SVGView implements IView {
     List<IShape> s = model.getShapeMap().keySet().stream().sorted(sortByAppear)
             .collect(Collectors.toList());
 
-
     // add shape xml to sb along with its list of events
     // will need to sort individual event lists by start time bc events must go inside shape tags
     for (IShape shp : s) {
       // if rect
       if (shp.getType().equalsIgnoreCase("rectangle")) {
         // add initial <rect> tag with attributes
-        String shapeStr = "<rect id=\"" + shp.getName() + "x=\"" + shp.getLocation().getX() +"\" y=\""
-        + shp.getLocation().getY() +"\" width=\"" + shp.getWidth() + "\" height=\""
-                + shp.getHeight() + "\" fill=rgb\"" + shp.getColor() + "\" visibility=\""
-        + "\"visible\" >\n";
-        sb.append(shapeStr);
+        sb.append(addRectangle(shp));
 
         // put the shape's events in a list, sorted by event time
         Comparator<IEvent> sortByEventBegin = Comparator.comparingInt(IEvent::getEventBegin);
-
         List<IEvent> t = model.getShapeMap().get(shp).stream().sorted(sortByEventBegin)
                 .collect(Collectors.toList());
 
         // add events for that shape
         for (IEvent e : t) {
-          int duration = e.getEventEnd() - e.getEventBegin();
-          String eventStr = "<animate attributeType=\"xml\" begin=\"" + e.getEventBegin() +"\" "
-                  + "\"dur=" + duration + "\" attributeName=\"" + shp.getType() + "\" from="
-                  + e.getBefore() + "\" to=\"" + e.getAfter() + "\" fill=\"freeze\" />";
+          addEvents(shp, e);
         }
-
         // add closing </rect>
         sb.append("</rect>\n");
       }
@@ -66,18 +58,18 @@ public class SVGView implements IView {
       // if ellipse
       if (shp.getType().equalsIgnoreCase("ellipse")) {
         // add initial <ellipse> tag with attributes
-        String shapeStr = "<ellipse id=\"" + shp.getName() + "cx=\"" + shp.getLocation().getX()
-                +"\" cy=\"" + shp.getLocation().getY() +"\" rx=\"" + shp.getWidth()/2 + "\" ry=\""
-                + shp.getHeight()/2 + "\" fill=rgb\"" + shp.getColor() + "\" visibility=\""
-                + "\"visible\" >";
-        sb.append(shapeStr);
+        sb.append(addEllipse(shp));
 
         // put the shape's events in a list, sorted by event time
         Comparator<IEvent> sortByEventBegin = Comparator.comparingInt(IEvent::getEventBegin);
-
+        List<IEvent> t = model.getShapeMap().get(shp).stream().sorted(sortByEventBegin)
+                .collect(Collectors.toList());
         // add events for that shape
-
+        for (IEvent e : t) {
+          addEvents(shp, e);
+        }
         // add closing </ellipse>
+        sb.append("</ellipse>\n");
       }
     }
 
@@ -88,5 +80,51 @@ public class SVGView implements IView {
     file.print(sb.toString());
     file.close();
 
+  }
+
+  private void addEvents(IShape shp, IEvent e) {
+    if (e.getEventType().equals(EventType.MOVE)) {
+      sb.append(addMove(shp, e));
+    }
+    if (e.getEventType().equals(EventType.SCALE)) {
+      sb.append(addScale(shp, e));
+    }
+    if (e.getEventType().equals(EventType.RECOLOR)) {
+      sb.append(addColorChange(shp, e));
+    }
+  }
+
+  private String addRectangle(IShape shp) {
+    String shapeStr = "<rect id=\"" + shp.getName() + "x=\"" + shp.getLocation().getX() +"\" y=\""
+            + shp.getLocation().getY() +"\" width=\"" + shp.getWidth() + "\" height=\""
+            + shp.getHeight() + "\" fill=rgb\"" + shp.getColor() + "\" visibility=\""
+            + "\"visible\" >\n";
+    return shapeStr;
+  }
+
+  private String addEllipse(IShape shp) {
+    String shapeStr = "<ellipse id=\"" + shp.getName() + "cx=\"" + shp.getLocation().getX()
+            +"\" cy=\"" + shp.getLocation().getY() +"\" rx=\"" + shp.getWidth()/2 + "\" ry=\""
+            + shp.getHeight()/2 + "\" fill=rgb\"" + shp.getColor() + "\" visibility=\""
+            + "\"visible\" >";
+    return shapeStr;
+  }
+
+  private String addMove(IShape shp, IEvent e) {
+    int duration = e.getEventEnd() - e.getEventBegin();
+    String eventStr = "<animate attributeType=\"xml\" begin=\"" + e.getEventBegin() +"\" "
+            + "\"dur=" + duration + "\" attributeName=\"" + shp.getType() + "\" from="
+            + e.getBefore() + "\" to=\"" + e.getAfter() + "\" fill=\"freeze\" />\n";
+    return eventStr;
+  }
+
+  private String addScale(IShape shp, IEvent e) {
+    String eventStr = "";
+    return eventStr;
+  }
+
+  private String addColorChange(IShape shp, IEvent e) {
+    String eventStr = "";
+    return eventStr;
   }
 }
