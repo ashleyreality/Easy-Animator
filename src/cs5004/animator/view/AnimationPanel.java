0cff3210c639e367fb299e73508fce18ba05673f
ashley <51171130+ashleyreality@users.noreply.github.com>
2021-04-13 17:06:21 -0400

    zzz

---
+++ src/cs5004/animator/view/AnimationPanel.java
@@ -0,0 +1,14 @@
+package cs5004.animator.view;
+
+import javax.swing.*;
+
+public class AnimationPanel extends JPanel {
+
+  // I think the Panel is supposed to be a separate class -- as seen in the Turtle code from mod10
+  // create a JPanel -- the window in which the animation happens
+  // https://docs.oracle.com/javase/tutorial/uiswing/components/panel.html
+
+  // create scroll panes so everything in the animation can be seen
+  // https://docs.oracle.com/javase/tutorial/uiswing/components/scrollpane.html
+
+}
--- src/cs5004/animator/view/VisualView.java
+++ src/cs5004/animator/view/VisualView.java
@@ -7,12 +7,7 @@ public class VisualView implements IView {
 
     // specify the speed
 
-    // I think the Panel is supposed to be a separate class -- as seen in the Turtle code from mod10
-    // create a JPanel -- the window in which the animation happens
-    // https://docs.oracle.com/javase/tutorial/uiswing/components/panel.html
 
-    // create scroll panes so everything in the animation can be seen
-    // https://docs.oracle.com/javase/tutorial/uiswing/components/scrollpane.html
 
     // create shapes
     // https://docs.oracle.com/javase/8/docs/api/java/awt/geom/Rectangle2D.Double.html
