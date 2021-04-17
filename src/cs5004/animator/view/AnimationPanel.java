f9ea4ac5acd0a842650bc5ab91b41a06f5ddbb62
aya :) <ayagare@gmail.com>
2021-04-17 01:29:08 -0400

    Updated AnimationPanel

--- src/cs5004/animator/view/AnimationPanel.java
+++ src/cs5004/animator/view/AnimationPanel.java
@@ -1,30 +1,29 @@
 package cs5004.animator.view;
 
-import java.awt.*;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
 import java.util.List;
 
-import javax.swing.*;
-
-import cs5004.animator.model.AnimationModelImpl;
 import cs5004.animator.model.IAnimationModel;
 import cs5004.animator.model.IShape;
 
 /**
- * This is the AnimationPanel class. It extends
+ * This is the AnimationPanel class. It extends the JPanel which is a container that can store a
+ * and organize group of components and provides access to override its paintComponent() method,
+ * which takes Graphics objects as parameters, and draws something on the JPanel using the super
+ * declaration.
  */
-public class AnimationPanel extends JPanel { // FixMe removed for now: implements ActionListener, ItemListener
+public class AnimationPanel extends JPanel {
   private int someTick;
   private IAnimationModel model;
 
-  // I think the Panel is supposed to be a separate class -- as seen in the Turtle code from mod10
   // create a JPanel -- the window in which the animation happens
   // https://docs.oracle.com/javase/tutorial/uiswing/components/panel.html
 
   // create scroll panes so everything in the animation can be seen
   // https://docs.oracle.com/javase/tutorial/uiswing/components/scrollpane.html
-
-  // should the constructor take in:
-  // IAnimationController myController, IAnimationModel myModel ?
 
   public AnimationPanel(IAnimationModel model, int someTick) {
     super();
@@ -37,35 +36,82 @@ public class AnimationPanel extends JPanel { // FixMe removed for now: implement
     super.paintComponent(g);
     Graphics2D g2d = (Graphics2D) g;
 
-    // Create a list of shapes at the given tick
-    List<IShape> listOfShapesAtSomeTick = model.getShapesAtTick(someTick);
+    // Create a list of rectangles at the given tick
+    List<IShape> listOfRectanglesAtSomeTick = model.getShapesAtTick(someTick);
     // Iterate through the list of shapes at the given tick
-    for (IShape eachShapeAtTick : listOfShapesAtSomeTick) {
-      //System.out.println("\n\n\nLIST OF SHAPES AT " + someTick);
-      //System.out.println(eachShapeAtTick);
+    for (IShape eachShapeAtTick : listOfRectanglesAtSomeTick) {
       // Check if it's a rectangle
       if (eachShapeAtTick.getType().equalsIgnoreCase("Rectangle")) { // FixMe to reliably check the shape type
-        int currentXLocation = (int) eachShapeAtTick.getLocation().getX();
-        int currentYLocation = (int) eachShapeAtTick.getLocation().getY();
-        int currentWidth = (int) eachShapeAtTick.getWidth();
-        int currentHeight = (int) eachShapeAtTick.getHeight();
-        int currentR = eachShapeAtTick.getColor().getRed();
-        int currentG = eachShapeAtTick.getColor().getGreen();
-        int currentB = eachShapeAtTick.getColor().getBlue();
-        // Draw the shape
-        g2d.drawRect(currentXLocation, currentYLocation, currentWidth, currentHeight);
-        g2d.setColor(new Color(currentR, currentG, currentB));
-        g2d.fillRect(currentXLocation, currentYLocation, currentWidth, currentHeight);
-
-        System.out.println("Tick: " + someTick + "  currentX: " + currentXLocation);
+        // Draw the rectangle(s)
+        drawRectangle(eachShapeAtTick, g2d);
+        setColor(eachShapeAtTick, g2d);
+        fillRectangleWithColor(eachShapeAtTick, g2d);
       }
-      //AYA TO DO: else if getType.equalsIgnoreCXase("Ellipse")
-      // {
-      //
-      // }
+      // Check if it's an oval/ellipse
+      if (eachShapeAtTick.getType().equalsIgnoreCase("Ellipse")
+              || eachShapeAtTick.getType().equalsIgnoreCase("Oval")) {
+        // Draw the oval(s)
+        drawEllipse(eachShapeAtTick, g2d);
+        setColor(eachShapeAtTick, g2d);
+        fillOvalWithColor(eachShapeAtTick, g2d);
+      }
     }
   }
 
+  private void setColor(IShape eachShapeAtTick, Graphics2D g2d) {
+    int currentR = eachShapeAtTick.getColor().getRed();
+    int currentG = eachShapeAtTick.getColor().getGreen();
+    int currentB = eachShapeAtTick.getColor().getBlue();
+    g2d.setColor(new Color(currentR, currentG, currentB));
+  }
+
+  private void fillRectangleWithColor(IShape eachShapeAtTick, Graphics2D g2d) {
+    int x = currentXLocation(eachShapeAtTick);
+    int y = currentYLocation(eachShapeAtTick);
+    int w = currentWidth(eachShapeAtTick);
+    int h = currentHeight(eachShapeAtTick);
+    g2d.fillRect(x, y, w, h);
+  }
+
+  private void fillOvalWithColor(IShape eachShapeAtTick, Graphics2D g2d) {
+    int x = currentXLocation(eachShapeAtTick);
+    int y = currentYLocation(eachShapeAtTick);
+    int w = currentWidth(eachShapeAtTick);
+    int h = currentHeight(eachShapeAtTick);
+    g2d.fillOval(x, y, w, h);
+  }
+
+  private void drawRectangle(IShape eachShapeAtTick, Graphics2D g2d) {
+    int x = currentXLocation(eachShapeAtTick);
+    int y = currentYLocation(eachShapeAtTick);
+    int w = currentWidth(eachShapeAtTick);
+    int h = currentHeight(eachShapeAtTick);
+    g2d.drawRect(x, y, w, h);
+  }
+
+  private void drawEllipse(IShape eachShapeAtTick, Graphics2D g2d) {
+    int x = currentXLocation(eachShapeAtTick);
+    int y = currentYLocation(eachShapeAtTick);
+    int w = currentWidth(eachShapeAtTick);
+    int h = currentHeight(eachShapeAtTick);
+    g2d.drawOval(x, y, w, h);
+  }
+
+  private int currentXLocation(IShape eachShapeAtTick) {
+    return (int) eachShapeAtTick.getLocation().getX();
+  }
+
+  private int currentYLocation(IShape eachShapeAtTick) {
+    return (int) eachShapeAtTick.getLocation().getY();
+  }
+
+  private int currentWidth(IShape eachShapeAtTick) {
+    return (int) eachShapeAtTick.getWidth();
+  }
+
+  private int currentHeight(IShape eachShapeAtTick) {
+    return (int) eachShapeAtTick.getHeight();
+  }
 
   public void setModel(IAnimationModel model) {
     this.model = model;
